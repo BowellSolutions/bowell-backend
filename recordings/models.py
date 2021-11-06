31c76394e2b45336ded5433fd437542e6e420dd2
Alschn <adam.lisichin@gmail.com>
2021-11-06 22:09:02 +0100

    BS-31 Replace explicit usage of User

--- recordings/models.py
+++ recordings/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 
 
@@ -6,7 +6,11 @@ class Recording(models.Model):
     file = models.FileField(upload_to='recordings')
     name = models.CharField(max_length=255)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    uploaded_by = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         return f'{self.name} ({self.id})'
