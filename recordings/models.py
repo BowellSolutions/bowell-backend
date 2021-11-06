dd9796f96344c64b0fb2546e4d4faefd87b5ae80
Wojciech Nowicki <wnowicki16@gmail.com>
2021-11-06 20:56:11 +0100

    BS-20 Handled GET, DELETE on recordings

--- recordings/models.py
+++ recordings/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,3 +6,7 @@ class Recording(models.Model):
     file = models.FileField(upload_to='recordings')
     name = models.CharField(max_length=255)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
--- recordings/views.py
+++ recordings/views.py
@@ -1,7 +1,22 @@
 from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
 
+from .models import Recording
 from .serializers import RecordingSerializer
 
 
-class RecordingUploadViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+class RecordingViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     serializer_class = RecordingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Recording.objects.filter(uploaded_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(uploaded_by=self.request.user)
