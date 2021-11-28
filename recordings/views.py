d563a455cddaec701f39c8d7e0d21eb0267a0f53
hubertDec <hubertdecyusz@gmail.com>
2021-11-28 22:56:35 +0100

    BS-20 adjusted views for model logic

--- recordings/views.py
+++ recordings/views.py
@@ -1,8 +1,9 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Recording
-from .serializers import RecordingSerializer
+from .serializers import RecordingBeforeAnalysisSerializer, RecordingAfterAnalysisSerializer, RecordingCreateSerializer
 
 
 class RecordingViewSet(
@@ -10,13 +11,32 @@ class RecordingViewSet(
     mixins.DestroyModelMixin,
     mixins.RetrieveModelMixin,
     mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
     viewsets.GenericViewSet
 ):
-    serializer_class = RecordingSerializer
+    serializer_class = RecordingBeforeAnalysisSerializer
     permission_classes = [IsAuthenticated]
+    queryset = Recording.objects.all()
 
-    def get_queryset(self):
-        return Recording.objects.filter(uploaded_by=self.request.user)
+    def get_serializer_class(self):
+        if hasattr(self, 'action') and self.action == 'create':
+            return RecordingCreateSerializer
+        elif hasattr(self, 'action') and self.action in ('update', 'partial_update', 'retrieve'):
+            return RecordingAfterAnalysisSerializer
+        return super().get_serializer_class()
 
-    def perform_create(self, serializer):
-        serializer.save(uploaded_by=self.request.user)
+    def destroy(self, request, *args, **kwargs):
+        examination_qs = self.get_object().examination_set
+        if examination_qs.exists():
+            examination = examination_qs.first()
+            examination.recording = None
+            examination.save(update_fields=['recording'])
+            return Response(
+                {'message': 'Recording was successfully detached from examination.'},
+                status=status.HTTP_204_NO_CONTENT
+            )
+        return Response(
+            {'message': 'Recording was not assigned to any examination.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
